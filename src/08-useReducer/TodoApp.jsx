7c1d8712532f859d2994521c37d2989bf5df79d1
Fernando Nicolás <fersanti2896@gmail.com>
2023-02-08 20:07:43 -0600

    Agregando y siendo persistente un todo en TodoApp.jsx

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,25 +1,35 @@
-import { useReducer } from 'react';
+import { useEffect, useReducer } from 'react';
 import { TodoAdd, TodoList } from './components';
 import { todoReducer } from "./todoReducer";
 
 const initialState = [
-    {
+    /* {
         id: new Date().getTime(),
         description: 'Recolectar la gema del infinito',
         done: false
-    },
-    {
-        id: new Date().getTime() * 3,
-        description: 'Recolectar la gema del poder',
-        done: false
-    }
+    } */
 ];
 
+const init = () => {
+    return JSON.parse( localStorage.getItem('todos') ) || [];
+}
+
 export const TodoApp = () => {
-    const [ todos, dispatch ] = useReducer( todoReducer, initialState );
+    const [ todos, dispatch ] = useReducer( todoReducer, initialState, init );
+    
+    useEffect(() => {
+        localStorage.setItem( 'todos', JSON.stringify(todos) );
+    }, [ todos ])
+    
 
     const handleNewTodo = ( todo ) => {
-        console.log({ todo });
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        }
+        
+        /* Se manda en la función del dispatch la acción */
+        dispatch( action );
     }
 
     return (
